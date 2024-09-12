eae1eb097e4a34b797877b95fabe2d9f91b57133
Maksims <sigmaligma@skibidi.com>
2024-09-12 10:50:31 +0300

    outer join testing

--- 2select-join/outer join.sql
+++ 2select-join/outer join.sql
@@ -1,12 +1,29 @@
 -- left outer join
 -- use sql_store; 
+-- SELECT 
+--     *
+-- FROM
+--     customers c
+--         LEFT JOIN
+--     orders o ON o.customer_id = c.customer_id;
+--     
+-- SELECT 
+--     *
+-- FROM
+--     orders o
+--         RIGHT JOIN
+--     customers c ON o.customer_id = c.customer_id;
+--  
+--  
+--  
+
+
 SELECT 
-    *
+    ot.quantity,
+    p.quantity_in_stock,
+    ot.order_id,
+    p.name,
+    p.product_id
 FROM
-    customers c
-        LEFT JOIN
-    orders o ON o.customer_id = c.customer_id;
-    
-select *
- from orders o 
- right join customers c on o.customer_id = c.customer_id;
+    order_items ot
+right join products p on ot.product_id = p.product_id; 
